79fc6cfe392a5ba67d1d803a3880abedc3580fb3
Hannes Polte <hannes.polte@gmx.de>
2021-11-18 09:42:35 +0100

    added the success and error page

--- client-frontend/src/components/pay-gateway/PaymentCancel.jsx
+++ client-frontend/src/components/pay-gateway/PaymentCancel.jsx
@@ -1,9 +1,31 @@
 import React from 'react';
+import cross_red from '../UI/cross_red.png';
+import { Link } from 'react-router-dom';
 
 const PaymentCancel = () => {
   return (
-    <div>
-      <h1>Hallo! Payment was canceled!</h1>
+    <div className='m-2 p-4 border border-black rounded flex flex-col text-center mt-6'>
+      <div className='mb-5'>
+        <h4 className='font-bold'>Payment Canceled!</h4>
+      </div>
+      <div>
+        <p>
+          Your Payment was canceled. Please go back to your cart and try again!
+        </p>
+      </div>
+      <div className='w-1/2 m-auto mt-10'>
+        <img src={cross_red} alt='successfulll green check' />
+      </div>
+      <div className='mt-4'>
+        <p>You can close this page now.</p>
+      </div>
+      <div className='mt-4'>
+        <Link to='/'>
+          <button className='bg-red-100 border rounded p-2 border-black'>
+            Close window
+          </button>
+        </Link>
+      </div>
     </div>
   );
 };
--- client-frontend/src/components/pay-gateway/PaymentSuccess.jsx
+++ client-frontend/src/components/pay-gateway/PaymentSuccess.jsx
@@ -1,9 +1,32 @@
 import React from 'react';
+import checkmark_green from '../UI/checkmark_green.png';
+import { Link } from 'react-router-dom';
 
 const PaymentSuccess = () => {
   return (
-    <div>
-      <h1>Hallo! PAyment was successfull!</h1>
+    <div className='m-2 p-4 border border-black rounded flex flex-col text-center mt-6'>
+      <div className='mb-5'>
+        <h4 className='font-bold'>Payment Successfull!</h4>
+      </div>
+      <div>
+        <p>
+          Your Payment was successfull and your Order is being processed right
+          now..
+        </p>
+      </div>
+      <div className='w-1/2 m-auto mt-10'>
+        <img src={checkmark_green} alt='successfulll green check' />
+      </div>
+      <div className='mt-4'>
+        <p>You can close this page now.</p>
+      </div>
+      <div className='mt-4'>
+        <Link to='/'>
+          <button className='bg-green-100 border rounded p-2 border-black'>
+            Close window
+          </button>
+        </Link>
+      </div>
     </div>
   );
 };
