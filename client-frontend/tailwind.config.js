c2fd27ea8a1a37837c004bd5aa85de22423615a9
Hannes Polte <hannes.polte@gmx.de>
2021-11-19 15:01:52 +0100

    changed the purge lines in the tailwind config file - added the components folder

--- client-frontend/tailwind.config.js
+++ client-frontend/tailwind.config.js
@@ -4,6 +4,9 @@ module.exports = {
     './src/components/pages/*.{js,jsx,ts,tsx}',
     './src/components/UI/*.{js,jsx,ts,tsx}',
     './public/index.html',
+    // new lines inserted afterwards:
+    './src/components/**/*.{js,ts,jsx,tsx}',
+    './src/components/**/**/*.{js,ts,jsx,tsx}',
   ],
   darkMode: 'class', // or 'media' or 'class' before false
   theme: {
@@ -14,3 +17,11 @@ module.exports = {
   },
   plugins: [],
 };
+
+// purge: [
+//   './pages/**/*.{js,ts,jsx,tsx}',
+//   './components/**/*.{js,ts,jsx,tsx}',
+//   './layout/**/*.{js,ts,jsx,tsx}',
+//   './helpers/**/*.{js,ts,jsx,tsx}',
+//   // Add more here
+// ],
